c467db813cf6d9cf1757ed026073a19aedf41358
Ugur <ugursmail@gmail.com>
2014-06-21 00:42:35 +0300

    hellou yazdim :))))))

--- PhotoUp/PUMasterViewController.m
+++ PhotoUp/PUMasterViewController.m
@@ -24,6 +24,7 @@
 
 - (void)viewDidLoad
 {
+    //heloou
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
