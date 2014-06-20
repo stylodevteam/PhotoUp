463a847523fc9def4f1d54efb8fb23e022408eef
Can Poyrazoğlu <Can@Cans-MacBook-Pro.local>
2014-06-21 01:26:23 +0300

    created account manager and added to pch

---
+++ PUAccountManager.h
@@ -0,0 +1,13 @@
+//
+//  PUAccountManager.h
+//  PhotoUp
+//
+//  Created by Can Poyrazoğlu on 21/06/14.
+//  Copyright (c) 2014 Peopleapps Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PUAccountManager : NSObject
+
+@end
---
+++ PUAccountManager.m
@@ -0,0 +1,13 @@
+//
+//  PUAccountManager.m
+//  PhotoUp
+//
+//  Created by Can Poyrazoğlu on 21/06/14.
+//  Copyright (c) 2014 Peopleapps Limited. All rights reserved.
+//
+
+#import "PUAccountManager.h"
+
+@implementation PUAccountManager
+
+@end
